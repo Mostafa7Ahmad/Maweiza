1bd9b1f9fe35f1592f7ffffbe240368e91ba864e
Mostafa Ahmad <101492635+Mostafa7Ahmad@users.noreply.github.com>
2023-10-08 18:44:01 +0200

    update pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 
+const runtimeCaching = require('next-pwa/cache');
+
 const withPWA = require('next-pwa')({
     dest: 'public',
+    disable: process.env.NODE_ENV === 'development', 
     register: true,
-    scope: '/app',
-    sw: 'service-worker.js',
-})
-
-module.exports = withPWA({
-    reactStrictMode: true,
+    skipWaiting: true,
+    runtimeCaching,
 });
+
+const nextConfig = withPWA({
+    reactStrictMode: false, 
+});
+
+module.exports = nextConfig;
