ce7a9d2f3852bd73670e26bd4c051dc45799d546
Mostafa Ahmad <101492635+Mostafa7Ahmad@users.noreply.github.com>
2023-10-09 10:02:02 +0200

    fix pwa

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
-/** @type {import('next').NextConfig} */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
 
 const runtimeCaching = require('next-pwa/cache');
-
 const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    runtimeCaching,
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development', // 👈 DISABLING PWA IN DEVELOPMENT MODE
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
 });
 
 const nextConfig = withPWA({
-    reactStrictMode: false, 
+  reactStrictMode: false, // 👈 DISABLING THIS TO AVOID DOUBLE RENDER
 });
 
 module.exports = nextConfig;
---
+++ src/app/fallback.js
@@ -0,0 +1,8 @@
+export default function fallback() {
+    return (
+        <div>
+            <h1>This is fallback page when device is offline </h1>
+            <small>Route will fallback to this page</small>
+        </div>
+    )
+};
