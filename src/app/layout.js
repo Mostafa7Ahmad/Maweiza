e8603964b3520268c1690d0301db1c7c4da0c8f0
mostafa ahmad <zn327855@gmail.com>
2025-02-28 17:19:00 +0200

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -59,45 +59,19 @@ export default function RootLayout({ children }) {
                     name="google-adsense-account"
                     content="ca-pub-2830940611983404"
                 />
-
-                <Script
-                    src="https://www.googletagmanager.com/gtag/js?id=G-WBZJG335"
-                    strategy="afterInteractive"
-                    async
-                />
-                <Script id="google-analytics" strategy="afterInteractive">
-                    {`
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', 'G-WBZJG335');
-                    `}
-                </Script>
-
-                <Script id="gtm-script" strategy="afterInteractive">
-                    {`
-                        (function(w,d,s,l,i){
-                        w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});
-                        var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                        j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                        f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-WBZJG335');
-                    `}
+                <Script id="google-analytics" strategy="lazyOnload">
+                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WBZJG335');`}
                 </Script>
             </head>
             <SWRProvider>
                 <body cz-shortcut-listen="true" className="bg-[rgb(250,250,250)] text-sm header dark:bg-[#000000f2] dark:text-white transition-colors selection:bg-lime-600 selection:text-white">
-                    <noscript>
-                        <iframe
-                            src="https://www.googletagmanager.com/ns.html?id=GTM-WBZJG335"
-                            height="0"
-                            width="0"
-                            style={{ display: 'none', visibility: 'hidden' }}
-                        />
-                    </noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-WBZJG335"
+                        height="0"
+                        width="0"
+                        title="google tag manager"
+                    >
+                    </iframe>
                     <main className={`${cairo.variable} relative font-cairo antialiased`}>
                         <Navbar />
                         {children}
